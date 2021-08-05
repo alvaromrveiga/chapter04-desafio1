c1923b51f35b590c92bfdbb630187c48504c02e1
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-08-05 19:29:58 -0300

    Export connection
    To fix app running before database is up and use the connection on the integration tests

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-(async () => await createConnection())();
+export default async (host = "database_chapter04"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+  console.log(defaultOptions.database);
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? defaultOptions.database + "_test"
+          : defaultOptions.database,
+    })
+  );
+};
