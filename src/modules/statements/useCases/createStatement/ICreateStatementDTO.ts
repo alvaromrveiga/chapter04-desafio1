5b8bef4530d436e85dbb62eb7d7478351923ba67
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-08-13 17:07:47 -0300

    Make description_id optional

--- src/modules/statements/useCases/createStatement/ICreateStatementDTO.ts
+++ src/modules/statements/useCases/createStatement/ICreateStatementDTO.ts
@@ -1,7 +1,12 @@
-export interface ICreateStatementDTO {
-  receiver_id?: string;
-  sender_id: string;
-  description: string;
-  amount: number;
-  type: string;
-}
+import { Statement } from "../../entities/Statement";
+
+type OptionalExceptFor<T, TRequired extends keyof T> = Partial<T> & Pick<T, TRequired>
+
+export type ICreateStatementDTO =
+OptionalExceptFor<
+  Statement,
+  'sender_id' |
+  'description' |
+  'amount' |
+  'type'
+>
