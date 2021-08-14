145976175d4749c8abedba0a0abbb638084bf659
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-08-14 13:28:41 -0300

    Rename to receiver_id

--- src/database/migrations/1628958279992-statements-rename-user_id-to-sender_id.ts
+++ src/database/migrations/1628958279992-statements-rename-user_id-to-sender_id.ts
@@ -4,10 +4,10 @@ export class statementsRenameUserIdToReceiverId1628882759094
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn("statements", "user_id", "receiver_id");
+    await queryRunner.renameColumn("statements", "user_id", "sender_id");
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn("statements", "receiver_id", "user_id");
+    await queryRunner.renameColumn("statements", "sender_id", "user_id");
   }
 }
